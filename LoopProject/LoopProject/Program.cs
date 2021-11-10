87c01a6819246dcfd5af758512d3d05fb4eeda20
junio <junetheprogrammer@gmail.com>
2021-11-10 15:54:26 -0500

    changes

--- LoopProject/LoopProject/Program.cs
+++ LoopProject/LoopProject/Program.cs
@@ -9,13 +9,13 @@ namespace LoopProject
     class Program
     {
         static void Main(string[] args)
-        {
+        {   // This is for a user to be promted to chose a number
             Console.WriteLine("Guess a number?");
             int number = Convert.ToInt32(Console.ReadLine());
             bool isGuessed = number == 12;
-
+            // this is a do (while ) statement that will aloow the loop to be satfsied at the begining
             do
-            {
+            {   // Here a user is promted to chose a number
                 switch (number)
                 {
                     case 62:
@@ -46,8 +46,29 @@ namespace LoopProject
 
             }
             while (!isGuessed);
-           
-            
+            // Here I am attempting to ADD a while loop witha boolean comparison
+
+            bool greatJob = true;
+            bool tryAgain = false;
+            while (tryAgain == false)
+            {
+                Console.Write("Did you win? Please answer true or false ");  // Asks if you won or not
+                greatJob = Convert.ToBoolean(Console.ReadLine());
+
+                if (greatJob)  // Validates the input containts characters and/or spaces
+                {
+                    Console.WriteLine("Great Job!!!!");
+                    tryAgain = true;
+                    
+                }
+                else  // Error message if the input is not valid
+                {
+                    
+                    Console.WriteLine("Try Again!");
+                    
+                }
+            }
+
             Console.Read();
         }
     }
---
+++ styringProject/styringProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace styringProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The man said, \"Hello\" Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            ////string fileName = "C:\\Users\\Jesse"; Or you could use @"C:\Users\Jesse"
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+            string slammed = "<Laying on the floor!>";
+            string threeWheel = lowLife.Style.ToShortDateString();
+
+            // Use the + and += operators for one-time concatenations.
+            string lowLife = "Lay on the floor " + slammed + ". Style is " + threeWheel + ".";
+            System.Console.WriteLine(lowLife);
+            
+            lowLife += "Hitting Switches on them";
+            System.Console.WriteLine(lowLife);
+
+            string parts = "Hydraulics";
+            parts = parts.ToUpper();
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+
+            StringBuilder lowriders = new StringBuilder();
+
+            lowriders.Append("Hit the switch");
+            lowriders.Append("Still Dippin");
+            lowriders.Append("And scrapping bumper!");
+
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
