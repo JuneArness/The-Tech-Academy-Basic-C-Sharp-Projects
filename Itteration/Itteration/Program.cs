2243dc153b3ed48ee35119fb52f743e05a9d58c8
junio <junetheprogrammer@gmail.com>
2021-11-16 09:57:34 -0500

    changes

--- Itteration/Itteration/Program.cs
+++ Itteration/Itteration/Program.cs
@@ -14,7 +14,7 @@ class Program
 
 
         Console.WriteLine("What is your score? Input Win, Lose or Draw:");
-        string playerScore = (Console.ReadLine());
+        string playerScore = Console.ReadLine();
 
         string[] scores = new string[3];
         scores[0] = "Win";
@@ -23,42 +23,48 @@ class Program
 
         Console.WriteLine("" + playerScore);
 
-        foreach (string score in playerScore) ;
+        foreach (string score in scores) 
         {
             Console.WriteLine();
         }
 
-        foreach (string score in playerScore)
+        foreach (string score in scores)
         {
             Console.WriteLine(playerScore);
         }
 
+
+
+        // --------
         int num02 = 0;
 
-        do
+        while (num02 == 0)
         {
-            Console.WriteLine("loop nr" + num02);
-            num02++;
-        } while(num02 < 10);
+            Console.WriteLine(num02 == 0);
+            break;
+        }
 
+
+
+        //-------
         int num03 = 0;
 
-        do
+        while (num03 < 10)
         {
-            Console.WriteLine("loop nr" + num03);
-            num03++;
-        } while (num03 < 10);
+            Console.WriteLine("the number is less than ten");
+            break;
+        }
 
         int num04 = 0;
 
-        do
+        while (num04 <= 10)
         {
-            Console.WriteLine("loop nr" + num04);
-            num04++;
-        } while (num04 <= 10);
+            Console.WriteLine("The number is less than or equals to ten");
+            break;
+        } 
 
 
-        
+        //--------
 
         List<int> list = new List<int>();
         list.Add(55);
@@ -68,51 +74,92 @@ class Program
         list.Add(73);
         list.Add(24);
         list.Add(65);
-
         Console.WriteLine("Chose from 55, 45, 55, 65, 73, 24, 65");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+
+        
         foreach (int a in list)
         {
-            Console.WriteLine("{0}", a);
-        }
-
-        List<int> myList = list.Distinct().ToList();
-        Console.WriteLine();
-        foreach (int a in myList)
-        {
-            Console.WriteLine("{0}", a);
-
-
-
-            int list;
-            if (int.TryParse(myList, out list))
-                Console.WriteLine($"Your number is: {list}");
+            if (list.Contains(userInput))
+            {
+                Console.WriteLine("The input has been found");
+                int index = list.IndexOf(userInput);
+                Console.WriteLine("number: " + userInput + " " + "index: " + index);
+                
+            }
             else
-                Console.WriteLine("This is not a number!");
-
-
-            Console.WriteLine("Please select a number 01, 01, 02, 03, 04, 04");
-            string[] x = new string[6];
-            x[0] = "01";
-            x[1] = "01";
-            x[2] = "02";
-            x[3] = "03";
-            x[4] = "04";
-            x[5] = "05";
-
-            List<string> x = new List<string>()
-                {
-                    "01",
-                    "01",
-                    "03",
-                    "04",
-                    "05",
-
-                };
-
-
-            Console.ReadLine();
-
-
+            {
+                Console.WriteLine("The input has not been found");
+                
+            }
+            break;
         }
+
+        //-----
+
+
+        List<int> x = new List<int>();
+        x[0] = (00);
+        x[1] = (01);
+        x[2] = (02);
+        x[3] = (03);
+        x[4] = (04);
+        x[5] = (05);
+
+        Console.WriteLine("Chose from 01, 01, 02, 03, 04, 05");
+        int numSelect = Convert.ToInt32(Console.ReadLine());
+
+
+        foreach (int a in x)
+        {
+            if (list.Contains(numSelect))
+            {
+                Console.WriteLine("The input has been found");
+                int append = list.IndexOf(numSelect);
+                Console.WriteLine("number: " + numSelect + " " + "append: " + append);
+
+            }
+            else
+            {
+                Console.WriteLine("The input has not been found");
+
+            }
+            break;
+        }
+
+
+
+        List<int> year = new List<int>();
+        year.Add(64);
+        year.Add(70);
+        year.Add(79);
+        year.Add(96);
+        year.Add(00);
+
+        Console.WriteLine("What is your favorite year of the Chevy Impala? Chose from '64, '70, '79, '96, '00");
+        int chevyImpala = Convert.ToInt32(Console.ReadLine());
+
+
+        foreach (int a in year)
+        {
+            if (list.Contains(chevyImpala))
+            {
+                Console.WriteLine("The input has been found");
+                int classes = list.IndexOf(chevyImpala);
+                Console.WriteLine("number: " + chevyImpala + " " + "append: " + classes);
+
+            }
+            else
+            {
+                Console.WriteLine("The input has not been found");
+
+            }
+            break;
+        }
+
+
+
+
+    }
 }
 
