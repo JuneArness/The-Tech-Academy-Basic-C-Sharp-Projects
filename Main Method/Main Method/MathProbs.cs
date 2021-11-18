3cd03c428b93e72aa7fbae0cadb29b620bd2695b
junio <junetheprogrammer@gmail.com>
2021-11-17 20:44:20 -0500

    changes

---
+++ Main Method/Main Method/MathProbs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class MathProbs
+    {
+        public int question1(int first )
+        {
+            return first + 5;
+        }
+
+        public int question2(decimal second)
+        {
+            int results = Convert.ToInt32(second);
+            return results - 10;
+        }
+
+        public int question3(string third)
+        {
+            int answer = Convert.ToInt32(third);
+            return answer + 30;
+        }
+    }
+}
---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // gathering the user input to apply to math equation 
+            MathProbs math = new MathProbs();
+
+            Console.WriteLine("Please enter a number:");
+            
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(math.question1(num1));
+
+            Console.WriteLine("Please enter a number:");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(math.question2(num2));
+
+            Console.WriteLine("Please enter a number:");
+            string num3 = (Console.ReadLine());
+            Console.WriteLine(math.question3(num3));
+
+            Console.ReadLine();
+        }
+    }
+}
